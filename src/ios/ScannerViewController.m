c94fdd8661a8d09f49e890f4631a2fb3e99ca383
winsigno <marwin.signo@tesi.com.ph>
2024-05-23 12:43:42 +0800

    Update ScannerViewController.m

--- src/ios/ScannerViewController.m
+++ src/ios/ScannerViewController.m
@@ -1,9 +1,4 @@
-//
-//  ScannerViewController.m
-//  BarcodeTestApp
-//
-//  Created by Carlos Correa on 26/04/2021.
-//
+// ScannerViewController.m
 
 #import "ScannerViewController.h"
 #import <AudioToolbox/AudioToolbox.h>
@@ -38,138 +33,42 @@
 
 #pragma mark - View Controller Methods
 
+// Initialization method with parameters
 -(instancetype)initWithScanInstructions:(NSString *)instructions CameraDirection:(CameraDirection)direction ScanOrientation:(ScanOrientation)orientation ScanLine:(bool)lineEnabled ScanButtonEnabled:(bool)buttonEnabled ScanButton:(NSString *)buttonTitle EnableAutoFocus:(BOOL)enableAutoFocus {
     self = [super initWithNibName:nil bundle:nil];
-    self.instructionsText = instructions;
-    self.scanButtonTitle = buttonTitle;
-    self.direction = direction;
-    self.orientation = orientation;
-    self.lineEnabled = lineEnabled;
-    self.scanButtonEnabled = buttonEnabled;
-    self.enableAutoFocus = enableAutoFocus;
+    if (self) {
+        self.instructionsText = instructions;
+        self.scanButtonTitle = buttonTitle;
+        self.direction = direction;
+        self.orientation = orientation;
+        self.lineEnabled = lineEnabled;
+        self.scanButtonEnabled = buttonEnabled;
+        self.enableAutoFocus = enableAutoFocus;
+    }
     return self;
 }
 
+// Dealloc method
 - (void)dealloc {
     [self.capture.layer removeFromSuperlayer];
 }
 
+// View did load
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    switch (self.orientation) {
-        case kPortrait:
-            [[UIDevice currentDevice] setValue:[NSNumber numberWithInt:UIDeviceOrientationPortrait] forKey:@"orientation"];
-            break;
-        case kLandscape:
-            [[UIDevice currentDevice] setValue:[NSNumber numberWithInt:UIDeviceOrientationPortrait] forKey:@"orientation"];
-            break;
-        default:
-            break;
-    }
-    
-    self.informationView.text = self.instructionsText;
-    
-    self.capture = [[ZXCapture alloc] init];
-    self.capture.sessionPreset = AVCaptureSessionPreset1920x1080;
-    switch (self.direction) {
-        case kBack:
-            self.capture.camera = self.capture.back;
-            break;
-        case kFront:
-            self.capture.camera = self.capture.front;
-            break;
-        default:
-            self.capture.camera = self.capture.back;
-            break;
-    }
-    self.capture.focusMode = AVCaptureFocusModeContinuousAutoFocus;
-    self.capture.delegate = self;
-    
-    self.scanning = NO;
-    
-    self.scanButton.layer.cornerRadius = 25;
-    self.scanButton.clipsToBounds = YES;
-    self.scanButton.imageView.contentMode = UIViewContentModeScaleAspectFill;
-    [self.scanButton setTitle:self.scanButtonTitle forState:UIControlStateNormal];
-    self.scanButton.hidden = !self.scanButtonEnabled;
-    
-    self.movingBar.hidden = !self.lineEnabled;
-    self.laserGradient.hidden = !self.lineEnabled;
-    
-    self.view.backgroundColor = UIColor.blackColor;
-    
-    UIView* cap = [[UIView alloc] init];
-    [cap setTranslatesAutoresizingMaskIntoConstraints:false];
-    [self.view addSubview:cap];
-    [[cap.topAnchor constraintEqualToAnchor:self.view.topAnchor] setActive:YES];
-    [[cap.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor] setActive:YES];
-    [[cap.rightAnchor constraintEqualToAnchor:self.view.rightAnchor] setActive:YES];
-    [[cap.leftAnchor constraintEqualToAnchor:self.view.leftAnchor] setActive:YES];
-    [cap.layer addSublayer:self.capture.layer];
-    
-    [self.view sendSubviewToBack:cap];
-    self.captureView = cap;
-    
-    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-}
-
-- (void)viewDidAppear:(BOOL)animated{
-    [super viewDidAppear:animated];
-    
-    self.capture.layer.frame = self.view.bounds;
-    
-    CGFloat scanRectRotation = [self getScanRotation];
-    self.capture.rotation = scanRectRotation;
-   
-    self.capture.layer.frame = self.view.bounds;
-    
-    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-    [self applyRectOfInterest:orientation];
+    // Setup UI elements
+    [self setupUI];
+    // Configure capture settings
+    [self configureCapture];
+    // Start capturing
+    [self startCapturing];
+    // Add gradient for moving laser effect
     [self addGradientToView];
-    [self animateMovingBar];
 }
 
+// View will appear
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    CGFloat captureRotation = [self getCaptureRotation];
-    CGAffineTransform transform = CGAffineTransformMakeRotation((CGFloat)(captureRotation / 180 * M_PI));
-    [self.capture setTransform:transform];
-    self.capture.layer.frame = UIScreen.mainScreen.bounds;
-    _laserGradient.hidden = false;
-}
-
--(UIInterfaceOrientationMask)supportedInterfaceOrientations{
-    switch (self.orientation) {
-        case kAdaptive:
-            return UIInterfaceOrientationMaskAll;
-        case kPortrait:
-            return UIInterfaceOrientationMaskPortrait;
-        case kLandscape:
-            return UIInterfaceOrientationMaskLandscape;
-        default:
-            return UIInterfaceOrientationMaskAll;
-    }
-}
-
-// Comment the added feature: Add gradient to the view for the moving laser effect
-- (void)addGradientToView{
-    if(!self.lineEnabled) { return; } // Check if line is enabled
-    // Create gradient layer for the moving laser effect
-    CAGradientLayer *gradientUp = [CAGradientLayer layer];
-    
-    // Set gradient colors
-    gradientUp.colors = @[(id)[UIColor colorWithWhite:1 alpha:0].CGColor,(id)[UIColor whiteColor].CGColor,(id)[UIColor whiteColor].CGColor,(id)[UIColor colorWithWhite:1 alpha:0].CGColor];
-    gradientUp.frame = self.laserGradient.bounds; // Set gradient frame
-    
-    // Set gradient locations
-    gradientUp.locations = @[@0.5, @0.5, @0.5, @0.5];
-    
-    // Insert gradient layer at index 0
-    [_laserGradient.layer insertSublayer:gradientUp atIndex:0];
-    
-    // Store the gradient layer
-    self.gradient = gradientUp;
-}
-
-// Comment the added feature:
+    // Adjust capture frame
+    [self adjustCaptureFrame];
+    // Show laser g
